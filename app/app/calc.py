8a608a6ae77112a60b84d13562a051811e5bae39
Alex Swiggum <alexswiggum@alexs-mbp.attlocal.net>
2023-06-12 20:59:20 -0500

    added postgresql database & configure unit tests

---
+++ app/app/calc.py
@@ -0,0 +1,17 @@
+"""
+Calculator Functions
+"""
+
+
+def add(x,y):
+    """
+    Add x & y and returns results.
+    """
+    return x + y
+
+
+def subtract(x, y):
+    """
+    Subtract x & y and return results
+    """
+    return y - x 
